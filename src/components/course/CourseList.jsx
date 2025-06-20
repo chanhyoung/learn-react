b76d278fba6896b6c3402b004abb75f2c71ff562
chanhyoung <chlee3927@gmail.com>
2025-06-20 18:06:53 +0900

    jsx 학습

---
+++ src/components/course/CourseList.jsx
@@ -0,0 +1,16 @@
+import CourseItem from "./CourseItem";
+
+export default function CourseList() {
+  return (
+    <div className="card">
+          <div className="card__header">강의 목록</div>
+          <div className="card__body">
+            <div className="courses">
+              <CourseItem></CourseItem>
+              <CourseItem></CourseItem>
+              <CourseItem></CourseItem>
+            </div>
+          </div>
+        </div>
+  );
+}
